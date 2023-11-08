4335f3934d007f8ab0e8a3eb99072f15cf6065db
A-upv <amora.hcs.est@gmail.com>
2023-11-08 10:28:27 +0100

    PC en el editor

--- Assets/Scripts/Efollow.cs
+++ Assets/Scripts/Efollow.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class Efollow : MonoBehaviour
 {
     public float speed = 1.0f;
-    [SerializeField]public GameObject target;
+    [SerializeField]public Transform target;
+   
+    public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        
     }
 }
